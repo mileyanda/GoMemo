22ea8cd4647fdf8cc4e5510f7781172284662393
mileyanda <yandaqa@gmail.com>
2017-03-14 20:26:40 +0700

    second commit

---
+++ app/src/main/res/values-v21/db/DatabaseOpenHelper.java
@@ -0,0 +1,29 @@
+package id.sch.smktelkom_mlg.project2.xirpl5182535.gomemo.com.javahelps.memo.db;
+
+/**
+ * Created by yanda on 14/03/2017.
+ */
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+class DatabaseOpenHelper extends SQLiteOpenHelper {
+    public static final String DATABASE = "memos.db";
+    public static final String TABLE = "memo";
+    public static final int VERSION = 1;
+
+    public DatabaseOpenHelper(Context context) {
+        super(context, DATABASE, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE memo(date INTEGER PRIMARY KEY, memo TEXT);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
